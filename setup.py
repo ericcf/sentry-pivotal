2f01cbd328ef98ca939acf0391fc3d38018fb47c
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-04-14 08:20:47 -0500

    release version 0.1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ install_requires = [
 
 setup(
     name='sentry-pivotal',
-    version='0.1.0',
+    version='0.1.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/getsentry/sentry-pivotal',
